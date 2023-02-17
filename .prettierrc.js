e22fea2c0f63f467c8fb2ecbabc7ede8123a6d3d
Daltonic <darlingtongospel@gmail.com>
2023-02-17 02:18:26 +0100

    first comit

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve',
+  endOfLine: 'lf',
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: false,
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
