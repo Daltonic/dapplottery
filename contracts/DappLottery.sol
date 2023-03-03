b354c7afd2c1a6482c162844d1218bafa7a1d370
Daltonic <darlingtongospel@gmail.com>
2023-03-03 03:05:44 +0100

    adds the create lottery function

---
+++ contracts/DappLottery.sol
@@ -0,0 +1,64 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract DappLottery {
+    using Counters for Counters.Counter;
+    Counters.Counter private _totalLotteries;
+
+    struct LotteryStruct {
+        uint256 id;
+        string title;
+        string description;
+        string image;
+        uint prize;
+        uint participants;
+        bool drawn;
+        uint createdAt;
+        uint expiresAt;
+    }
+
+    struct ParticipantStruct {
+        address account;
+        string lotteryNumber;
+    }
+
+    uint256 serviceFee;
+    uint256 serviceAccount;
+
+
+    mapping(uint256 => LotteryStruct) lotteries;
+    mapping(uint256 => ParticipantStruct[]) lotteryParticipants;
+    mapping(uint256 => string[]) lotteryLuckyNumbers;
+
+    function createLottery(
+        string memory title,
+        string memory description,
+        string memory image,
+        uint256 expiresAt
+    ) public payable {
+        require(bytes(title).length > 0, "title cannot be empty");
+        require(bytes(description).length > 0, "description cannot be empty");
+        require(bytes(image).length > 0, "image cannot be empty");
+        require(expiresAt > block.timestamp, "expireAt cannot be less than the future");
+        require(msg.value > 0 ether, "prize cannot be zero");
+
+        _totalLotteries.increment();
+        LotteryStruct memory lottery;
+
+        lottery.id = _totalLotteries.current();
+        lottery.title = title;
+        lottery.description = description;
+        lottery.image = image;
+        lottery.prize = msg.value;
+        lottery.createdAt = block.timestamp;
+        lottery.expiresAt = expiresAt;
+    }
+
+    function importLuckyNumbers(uint256 id, string[] memory luckyNumbers) public {
+        require(luckyNumbers.length > 0, "Lucky numbers cannot be zero");
+        lotteryLuckyNumbers[id] = luckyNumbers;
+    }
+}
