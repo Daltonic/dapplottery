f02930291d6363e8c657fd4db04255c843f90f47
Daltonic <darlingtongospel@gmail.com>
2023-03-03 03:51:14 +0100

    Adds the ability to randomly select winners

--- contracts/DappLottery.sol
+++ contracts/DappLottery.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract DappLottery {
+contract DappLottery is Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _totalLotteries;
 
@@ -13,17 +13,24 @@ contract DappLottery {
         string title;
         string description;
         string image;
-        uint prize;
-        uint ticketPrice;
-        uint participants;
+        uint256 prize;
+        uint256 ticketPrice;
+        uint256 participants;
         bool drawn;
-        uint createdAt;
-        uint expiresAt;
+        uint256 createdAt;
+        uint256 expiresAt;
     }
 
     struct ParticipantStruct {
         address account;
         string lotteryNumber;
+    }
+
+    struct LotteryResultStruct {
+        uint256 id;
+        bool completed;
+        uint256 timestamp;
+        ParticipantStruct[] winners;
     }
 
     uint256 serviceFee;
@@ -33,6 +40,7 @@ contract DappLottery {
     mapping(uint256 => ParticipantStruct[]) lotteryParticipants;
     mapping(uint256 => string[]) lotteryLuckyNumbers;
     mapping(uint256 => mapping(uint256 => bool)) luckyNumberUsed;
+    mapping(uint256 => LotteryResultStruct) lotteryResult;
 
     function createLottery(
         string memory title,
@@ -45,7 +53,10 @@ contract DappLottery {
         require(bytes(description).length > 0, "description cannot be empty");
         require(bytes(image).length > 0, "image cannot be empty");
         require(ticketPrice > 0 ether, "ticketPrice cannot be zero");
-        require(expiresAt > block.timestamp, "expireAt cannot be less than the future");
+        require(
+            expiresAt > block.timestamp,
+            "expireAt cannot be less than the future"
+        );
         require(msg.value > 0 ether, "prize cannot be zero");
 
         _totalLotteries.increment();
@@ -60,15 +71,23 @@ contract DappLottery {
         lottery.expiresAt = expiresAt;
     }
 
-    function importLuckyNumbers(uint256 id, string[] memory luckyNumbers) public {
+    function importLuckyNumbers(uint256 id, string[] memory luckyNumbers)
+        public
+    {
         require(luckyNumbers.length > 0, "Lucky numbers cannot be zero");
         lotteryLuckyNumbers[id] = luckyNumbers;
     }
 
     function buyTicket(uint256 id, uint256 luckyNumberId) public payable {
         require(lotteries[id].id == id, "Lottery not found");
-        require(!luckyNumberUsed[id][luckyNumberId], "Lucky number already used");
-        require(msg.value >= lotteries[id].ticketPrice, "insufficient ethers to buy ethers");
+        require(
+            !luckyNumberUsed[id][luckyNumberId],
+            "Lucky number already used"
+        );
+        require(
+            msg.value >= lotteries[id].ticketPrice,
+            "insufficient ethers to buy ethers"
+        );
 
         lotteries[id].participants++;
         lotteryParticipants[id].push(
@@ -78,5 +97,32 @@ contract DappLottery {
             )
         );
         luckyNumberUsed[id][luckyNumberId] = true;
+    }
+
+    function randomlySelectLuckyNumbers(uint256 id, uint256 numberOfWinners)
+        public
+        onlyOwner
+    {
+        require(lotteries[id].id == id, "Lottery not found");
+        require(!lotteryResult[id].completed, "Lottery have already been completed");
+        require(
+            numberOfWinners <= lotteryParticipants[id].length,
+            "Number of winners exceeds number of participants"
+        );
+
+        ParticipantStruct[] storage participants = lotteryParticipants[id];
+
+        for (uint256 i = 0; i < numberOfWinners; i++) {
+            uint256 index = uint256(keccak256(abi.encodePacked(block.timestamp, i))) % participants.length;
+            lotteryResult[id].winners[i] = participants[index];
+
+            // Remove the selected participant from the list to prevent them from being selected again
+            participants[index] = participants[participants.length - 1];
+            participants.pop();
+        }
+
+        lotteryResult[id].id = id;
+        lotteryResult[id].completed = true;
+        lotteryResult[id].timestamp = block.timestamp;
     }
 }
